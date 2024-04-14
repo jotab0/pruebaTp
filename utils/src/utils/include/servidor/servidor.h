33cbeb576ed935ef4507c6fa02bebe5bc85066e2
camilazzati <clazzati@frba.utn.edu.ar>
2024-04-14 19:33:47 +0000

    por ahora todo lo de cliente-servidor'

--- utils/src/utils/include/servidor/servidor.h
+++ utils/src/utils/include/servidor/servidor.h
@@ -11,6 +11,32 @@
 #include<string.h>
 #include<assert.h>
 
+// despues se van a ir agregando mas
+typedef enum
+{
+	MENSAJE,
+	PAQUETE
+    //HANDSHAKE,
+    //RTA_HANDSHAKE,
+    //------- KERNEL MEMORIA----- (poner esto para tenerlo mas ordenado)
+    //CREAR_PROCESO_KM 
+    //RTA_CREAR_PROCESO_MK
+    //------- KERNEL CPU --------
+    // etc
+}op_code;
+
+// para enviar y recibir mensajes
+typedef struct{
+    int size;
+    void* stream;
+}t_buffer;
+
+//estructura que tiene adentro otra estructura(buffer)
+typedef struct{
+    op_code codigo_operacion;
+    t_buffer* buffer;
+}t_paquete;
+
 int iniciar_servidor(char* puerto, t_log* log, char* msj_del_server); 
 int esperar_cliente(int socket_servidor, t_log* log);
 int recibir_operacion(int socket_cliente);
